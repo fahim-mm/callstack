85b5492cfbb9013600f797ec895acfaa8915cd3a
fahim-mm <kefayatullahfahim@@gmail.com>
2022-11-07 01:06:47 +0600

    nested function

---
+++ callstack.js
@@ -0,0 +1,15 @@
+let x = 30;
+const y = 50;
+const u = (y) => {
+     x = x+10;
+     const j=()=>{
+        const e=11;
+        x=x+20;
+     }
+     j();
+    const b = 70;
+    const w = 90;
+}
+const o = 20;
+u(5);
+u(10);
